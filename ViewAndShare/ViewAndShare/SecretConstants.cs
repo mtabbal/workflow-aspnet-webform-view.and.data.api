5403a49b9b75dfe4880b892b69fd395afc68ae82
Daniel Du <duchangyu@gmail.com>
2014-07-03 16:45:56 +0800

    First commit
    workflow sample, upload/translate model files, view it in viewer, share
    with friends

---
+++ ViewAndShare/ViewAndShare/SecretConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewAndShare
+{
+    public class SecretConstants
+    {
+        public static string BASE_URL = "https://developer.api.autodesk.com";
+        public static string DEFAULT_BUCKET_KEY = "daniel_translate_bucket2";
+
+        public static string CLIENT_ID = "<your consumer key>";
+        public static string CLIENT_SECRET = "<your secret key>";
+    }
+}
