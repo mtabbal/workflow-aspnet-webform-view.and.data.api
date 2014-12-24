ab1ed9d0dd910adef35a022b9f8c286eab20f383
Daniel Du <duchangyu@gmail.com>
2014-12-24 15:42:30 +0800

    add crendentials

---
+++ ViewAndShare/ViewAndShare/Credentials_.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewAndShare
+{
+    public class Credentials
+    {
+        public static string BASE_URL = "https://developer.api.autodesk.com";
+        public static string DEFAULT_BUCKET_KEY = "your_bucket_name";
+
+        //get your consumer key and the secret key from http://developer.autodesk.com 
+        public static string CONSUMER_KEY = "your consumer key";
+        public static string CONSUMER_SECRET = "your secret key";
+
+        //
+
+        
+    }
+}
